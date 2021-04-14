c774230597462fadbc147ed809ce90ab24d68b96
PENG GAO <gaopeng19931027@gmail.com>
2021-04-14 15:44:08 +1000

    add files

---
+++ testdb.sql
@@ -0,0 +1,28 @@
+-- admin table
+CREATE TABLE `users_admin` (
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type_user` varchar(100) NOT NULL,
+    `username` varchar(100) NOT NULL,
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `mobile` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    `address` varchar(100) NOT NULL,
+    `country` varchar(100) NOT NULL,
+    `abn` varchar(100) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+
+-- customer table
+CREATE TABLE `users_customer` (
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` varchar(100) NOT NULL,
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `mobile` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    `address` varchar(100) NOT NULL,
+    `country` varchar(100) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
