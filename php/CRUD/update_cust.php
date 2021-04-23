839e7bbdfd3c4ea29ad3a5e7faf9828772d232fe
PENG GAO <gaopeng19931027@gmail.com>
2021-04-23 11:09:31 +1000

    chenges 23

--- php/CRUD/update_cust.php
+++ php/CRUD/update_cust.php
@@ -1,11 +1,55 @@
-<?php include('update_cust_f.php') ?>
+<?php //include('update_cust_f.php') 
+?>
+
+<?php
+session_start();
+include "../../db_conn.php";
+$c_username = "";
+$c_first_name = "";
+$c_last_name = "";
+$c_email = "";
+$c_mobile = "";
+$c_address = "";
+$c_country = "";
+$errors = array();
+
+if (isset($_GET['id'])) {
+        // include "../../db_conn.php";
+
+        function validate($data)
+        {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+        }
+
+        $id = validate($_GET['id']);
+
+        $sql = "SELECT * FROM users_customer WHERE id=$id";
+        $result = mysqli_query($db, $sql);
+        if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                // $c_username = $row['c_username'];
+                // $c_first_name = $row['c_first_name'];
+                // $c_last_name = $row['c_last_name'];
+                // $c_email = $row['c_email'];
+                // $c_mobile = $row['c_mobile'];
+                // $c_address = $row['c_address'];
+                // $c_country = $row['c_country'];
+        } else {
+                // echo ("ok");
+                header("Location: ../sys_dashboard.php");
+        }
+}
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
         <title>Update</title>
-        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- <link rel="stylesheet" href="css/style.css"> -->
         <link rel="stylesheet" type="text/css" href="../../css/style.css">
 </head>
 
@@ -13,40 +57,45 @@
         <div class="header">
                 <h2>Update customer</h2>
         </div>
-        <form method="post" action="update_cust.php">
+        <form method="post" action="update_cust_f.php">
 
+                <?php if (isset($_GET['error'])) { ?>
+                        <div class="alert alert-danger" role="alert">
+                                <?php echo $_GET['error']; ?>
+                        </div>
+                <?php } ?>
 
-                <?php include('../../errors.php'); ?>
+                <input type="text" name="id" value="<?php echo $id; ?>" ; hidden>
                 <div class="input-group">
                         <label>Username</label>
-                        <input type="text" name="c_username" value="<?php echo $c_username; ?>">
+                        <input type="text" name="c_username" value="<?= $row['c_username'] ?>">
                 </div>
 
                 <div class="input-group">
                         <label>First Name</label>
-                        <input type="text" name="c_first_name" value="<?php echo $c_first_name; ?>">
+                        <input type="text" name="c_first_name" value="<?= $row['c_first_name'] ?>">
                 </div>
 
                 <div class="input-group">
                         <label>Last Name</label>
-                        <input type="text" name="c_last_name" value="<?php echo $c_last_name; ?>">
+                        <input type="text" name="c_last_name" value="<?= $row['c_last_name'] ?>">
                 </div>
                 <div class="input-group">
                         <label>Email</label>
-                        <input type="email" name="c_email" value="<?php echo $c_email; ?>">
+                        <input type="email" name="c_email" value="<?= $row['c_email'] ?>">
                 </div>
 
                 <div class="input-group">
                         <label>Mobile</label>
-                        <input type="text" name="c_mobile" value="<?php echo $c_mobile; ?>">
+                        <input type="text" name="c_mobile" value="<?= $row['c_mobile'] ?>">
                 </div>
                 <div class="input-group">
                         <label>Address</label>
-                        <input type="text" name="c_address" value="<?php echo $c_address; ?>">
+                        <input type="text" name="c_address" value="<?= $row['c_address'] ?>">
                 </div>
                 <div class="input-group">
                         <label>Country</label>
-                        <select class="c_country" name="c_country" value="<?php echo $c_country; ?>"><br>
+                        <select class="c_country" name="c_country" value="<?= $row['c_country'] ?>"><br>
                                 <option selected value=""></option>
                                 <option value="China">China</option>
                                 <option value="Australia">Australia</option>
@@ -57,17 +106,11 @@
                         </select><br>
                 </div>
                 <div class="input-group">
-                
-                        <button type="submit" class="btn btn-primary" name="update_cust_ad">Update</button>
+
+                        <button type="submit" class="btn btn-primary" name="edit_cust">Update</button>
                         <a href="../sys_dashboard.php" class="link-primary">View</a>
                 </div>
         </form>
 </body>
 
 </html>
-
-
-<td><a href="update.php?id=<?= $rows['id'] ?>" class="btn btn-success">Update</a>
-
-<a href="php/delete.php?id=<?= $rows['id'] ?>" class="btn btn-danger">Delete</a>
-</td>
